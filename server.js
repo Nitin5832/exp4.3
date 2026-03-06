9b9196d38c4328bbb5fd3471cee7acfd6e71854a
Rashmika Sharma <rashmikasharma913@gmail.com>
2026-03-06 22:58:19 +0530

    ticket booking redis project

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Redis } from "@upstash/redis";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+});
+
+app.post("/book", async (req, res) => {
+
+  const seat = req.body.seat;
+
+  const lock = await redis.set(seat, "locked", {
+    nx: true,
+    ex: 30
+  });
+
+  if (!lock) {
+    return res.json({ message: "Seat already booked" });
+  }
+
+  res.json({ message: "Seat booked successfully" });
+
+});
+
+app.get("/", (req,res)=>{
+  res.send("Server is working");
+});
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
